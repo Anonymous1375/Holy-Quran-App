3875bf0ef9c1529f9193c4a14d41f9d8541e7f52
Anonymous1375 <uwaisk55@gmail.com>
2026-06-13 05:49:15 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quran-v9';
+const CACHE = 'quran-v10';
 const SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
