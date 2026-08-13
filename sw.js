52d482a0ec61536e9fb9ff89de6b056a2c3641c9
Anonymous1375 <uwaisk55@gmail.com>
2026-08-13 22:49:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quran-v10.5';
+const CACHE = 'quran-v10.6'; // v10.6: invalidates cached images so corrected pages 0401-0430/0529 reload
 const SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
